b5326dc1d291a6091770118332bce469ac33e32c
AnaLuizanc <analuizanc13@gmail.com>
2024-12-13 17:57:34 -0300

    create alloc method and test it in main function

--- main.cpp
+++ main.cpp
@@ -11,7 +11,27 @@ class SimpleMemoryManager {
             memory.assign(tamanho, 0);
         }
 
-        
+        int alloc(int n){
+            int largestSequence0 = 0;
+            int indice = 0;
+            for(int i=0; i<memory.size(); i++){
+                if(memory[i] == 0)
+                    ++largestSequence0;
+                else{
+                    largestSequence0 = 0;
+                    indice = i;
+                }
+            }
+            if(n < largestSequence0){
+                if(indice != 0)
+                    indice++;
+                for(int i = indice; i < n+indice; i++){
+                    memory[i] = 1;
+                }
+                return indice;
+            }else
+                return -1;
+        }
 
 
 };
@@ -19,7 +39,18 @@ class SimpleMemoryManager {
 int main(){
     SimpleMemoryManager mem(10);
 
-    
+    for(int i=0; i<mem.memory.size(); i++)
+        cout << mem.memory[i]; 
+
+    int indice = mem.alloc(3);
+    cout << endl << "Alocacao bem sucedida, iniciando no indice " << indice << endl;
+    for(int i=0; i<mem.memory.size(); i++)
+        cout << mem.memory[i]; 
+
+    indice = mem.alloc(4);
+    cout << endl << "Alocacao bem sucedida, iniciando no indice " << indice << endl;
+    for(int i=0; i<mem.memory.size(); i++)
+        cout << mem.memory[i]; 
 
 
     return 0;
